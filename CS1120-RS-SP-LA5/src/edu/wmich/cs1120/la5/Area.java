7811577dae6e9947a272619ae1ee087688dc5da7
Richard <Richard@10.80.199.89>
2017-03-16 08:53:38 -0400

    Edited the Area Class.

--- CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/Area.java
+++ CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/Area.java
@@ -15,6 +15,53 @@ public abstract class Area implements IArea {
 		this.elevation = elevation;
 		this.radiation = radiation;
 	}
+	
+	// *************************************************************************
+	@Override
+	public double getBasicEnergyCost() {
+		
+		
+		return basicEnergyCost;
+	}
+	
+	// *************************************************************************
+	@Override
+	public void setBasicEnergyCost(double basicEnergyCost) {
+		
+		this.basicEnergyCost = basicEnergyCost;
+	}
+	
+	// *************************************************************************
+	@Override
+	public double getElevation() {
+		
+		return elevation;
+	}
+	
+	// *************************************************************************
+	@Override
+	public void setElevation(double elevation) {
+		
+		this.elevation = elevation;
+	}
+	
+	// *************************************************************************
+	@Override
+	public double getRadiation() {
+		
+		return radiation;
+	}
+	
+	// *************************************************************************
+	@Override
+	public void setRadiation(double radiation) {
+		
+		this.radiation = radiation;
+	}
+	
+	// *************************************************************************
+	@Override
+	public abstract double calcConsumedEnergy();
 	// *************************************************************************
 	
 }
