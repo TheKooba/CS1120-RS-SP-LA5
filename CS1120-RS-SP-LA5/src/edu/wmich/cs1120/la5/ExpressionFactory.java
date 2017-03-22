0d508d9483269ca173be53818cc5d03e2b5e7b45
Richard <richard.j.schaefer@wmich.edu>
2017-03-22 11:28:22 -0400

    Worked on ExpressionFactory Class

--- CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
+++ CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/ExpressionFactory.java
@@ -6,18 +6,30 @@ public class ExpressionFactory {
 		
 	}
 	
-	// This method will create and return a proper object
-	// according to the arguments received
 	// *************************************************************************
 	/**
-	 * 
-	 * @param operator
-	 * @param val1
-	 * @param val2
+	 * This method will create and return a proper object according 
+	 * to the arguments received
+	 * @param operator The operator signifies addition or subtraction
+	 * @param val1 Value One for the operation
+	 * @param val2Value Two for the operation
 	 * @return
 	 */
 	public static IExpression getExpression(char operator, int val1, int val2) {
 		
+		Addition add = new Addition();
+		Subtraction subtract = new Subtraction();
+		
+		Literal a = new Literal(val1);
+		Literal b = new Literal(val2);
+		
+		if (operator == '+') {
+			IExpression exp = new BinaryExpression(add, a, b);
+		}
+		
+		if (operator == '-') {
+			IExpression exp = new BinaryExpression(subtract, a, b);
+		}
 		
 		return null;
 	}
