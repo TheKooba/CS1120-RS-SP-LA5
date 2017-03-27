421101b59bb0b75929b663fa707db3737ac5c227
Richard <richard.j.schaefer@wmich.edu>
2017-03-27 12:22:35 -0400

    Made a few changes to MapCreatorFromDat Class

--- CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
+++ CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/MapCreatorFromDat.java
@@ -1,13 +1,9 @@
 package edu.wmich.cs1120.la5;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.util.Scanner;
 
 public class MapCreatorFromDat implements IMapCreator {
 	
@@ -24,7 +20,6 @@ public class MapCreatorFromDat implements IMapCreator {
 	@Override
 	public void scanTerrain(String fileName, int threshold) throws IOException {
 		
-
 		// Initialize & set up the Scanner / File Reader
 		File file = new File(fileName);
 		DataInputStream scan = new DataInputStream(new FileInputStream (file));
@@ -35,8 +30,7 @@ public class MapCreatorFromDat implements IMapCreator {
 		double radiation = 0;
 
 
-// Reads the .dat file
-			
+			// For-Loop reads the .dat file
 			for (int i = 0; i < 10; i++){
 				for (int j = 0; j < 10; j++){
 					basicEnergyCost = scan.readDouble();
@@ -56,6 +50,7 @@ public class MapCreatorFromDat implements IMapCreator {
 					}
 				}
 			}
+			
 			scanner.setTerrain(areaArray);
 			scan.close();
 		}
