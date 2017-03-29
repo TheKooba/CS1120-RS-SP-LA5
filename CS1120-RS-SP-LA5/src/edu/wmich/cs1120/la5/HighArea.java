853798c6b50744053117a33f94891e836cb35dcf
kert8 <kert8@10.80.145.67>
2017-03-29 10:37:29 -0400

    Fixed Low and High Area

--- CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/HighArea.java
+++ CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/HighArea.java
@@ -18,70 +18,7 @@ public class HighArea extends Area {
 		super(basicEnergyCost, elevation, radiation);
 	}
 	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public double getBasicEnergyCost() {
-		
-		return basicEnergyCost;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public void setBasicEnergyCost(double basicEnergyCost) {
-		
-		this.basicEnergyCost = basicEnergyCost;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public double getElevation() {
-		
-		return elevation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public void setElevation(double elevation) {
-		
-		this.elevation = elevation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public double getRadiation() {
-		
-		return radiation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public void setRadiation(double radiation) {
-		
-		this.radiation = radiation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
+
 	@Override
 	public double calcConsumedEnergy() {
 		
--- CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/LowArea.java
+++ CS1120-RS-SP-LA5/src/edu/wmich/cs1120/la5/LowArea.java
@@ -17,71 +17,7 @@ public class LowArea extends Area {
 		
 		super(basicEnergyCost, elevation, radiation);
 	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public double getBasicEnergyCost() {
-		
-		return basicEnergyCost;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public void setBasicEnergyCost(double basicEnergyCost) {
-		
-		this.basicEnergyCost = basicEnergyCost;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public double getElevation() {
-		
-		return elevation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public void setElevation(double elevation) {
-		
-		this.elevation = elevation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public double getRadiation() {
-		
-		return radiation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
-	@Override
-	public void setRadiation(double radiation) {
-		
-		this.radiation = radiation;
-	}
-	
-	// *************************************************************************
-	/**
-	 * 
-	 */
+
 	@Override
 	public double calcConsumedEnergy() {
 		
